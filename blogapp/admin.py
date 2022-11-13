943842595f074c3ed3b507d0dbd93f7178287b3b
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-11-13 12:12:44 +0900

    add: Article model and admin

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from blogapp.models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'num_read', 'is_public')
+
+
+admin.site.register(Article, ArticleAdmin)
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    num_read = models.IntegerField(default=0)
+    created = models.DateField(default=datetime.date.today)
+    is_public = models.BooleanField(default=False)
